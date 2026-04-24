a5af886d07cd60df42b0663905281c1ffeadf050
s-blu <git@s-blu.de>
2026-04-24 15:10:25 +0200

    refactor(showcase): place showcase above garden

--- src/main.ts
+++ src/main.ts
@@ -21,14 +21,14 @@ app.innerHTML = `
     <p class="msg-bar" id="msg">${t.welcomeMsg}</p>
   </header>
 
-  <section>
-    <p class="section-title">${t.sectionGarden}</p>
-    <div class="pots-row" id="pots-row"></div>
-  </section>
-
   <section id="showcase-section" style="display:none">
     <p class="section-title">${t.sectionShowcase}</p>
     <div class="pots-row" id="showcase-row"></div>
+  </section>
+
+  <section>
+    <p class="section-title">${t.sectionGarden}</p>
+    <div class="pots-row" id="pots-row"></div>
   </section>
 
   <section>
